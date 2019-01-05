1e7969f063ba61078f5c872f267e4ae5a168b138
Wouter Kursten <wouter@retouw.nl>
2019-01-05 10:41:19 +0100

    cleanup

--- Plugins/30 Pods/00 Pod Health.ps1
+++ Plugins/30 Pods/00 Pod Health.ps1
@@ -5,14 +5,15 @@ $podhealth=@()
 foreach ($pod in $pods) {
 	$endpoints=$services1.podhealth.podhealth_get($pod.id).data.endpointhealth
 	foreach ($endpoint in $endpoints){
-		$podhealth+= New-Object PSObject -Property @{"Remote_POD_Name" = $services1.pod.pod_get($pod.id) | select -expandproperty DisplayName
-		"Endpoint_Name" = $endpoint.EndpointInfo | select -expandproperty Name;
-		"Endpoint_Address" = $endpoint.EndpointInfo | select -expandproperty ServerAddress;
-		"Status" = $endpoint.EndpointInfo | select -expandproperty Enabled;
-		"State" = $endpoint | select -expandproperty State;
-		"Roundtrip_Time" = $endpoint | select -expandproperty RoundTripTime;
-}	
-}	
+		$podhealth+= New-Object PSObject -Property @{
+			"Remote_POD_Name" = $services1.pod.pod_get($pod.id) | select -expandproperty DisplayName
+			"Endpoint_Name" = $endpoint.EndpointInfo | select -expandproperty Name;
+			"Endpoint_Address" = $endpoint.EndpointInfo | select -expandproperty ServerAddress;
+			"Status" = $endpoint.EndpointInfo | select -expandproperty Enabled;
+			"State" = $endpoint | select -expandproperty State;
+			"Roundtrip_Time" = $endpoint | select -expandproperty RoundTripTime;
+		}	
+	}	
 }
 $podhealth | select Remote_POD_Name,Endpoint_Name,Endpoint_Address,Status,State,Roundtrip_Time
 
@@ -21,5 +22,5 @@ $Header = "VDI Remote Pod Health"
 $Comments = "This is the check to see if all Pod Communications are ok"
 $Display = "Table"
 $Author = "Wouter Kursten"
-$PluginVersion = 0.1.1
+$PluginVersion = 0.1.2
 $PluginCategory = "View"
