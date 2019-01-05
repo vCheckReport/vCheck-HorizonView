8c3c3440fcc28440c686f6fbe96ebc1a8a0e1052
Wouter Kursten <wouter@retouw.nl>
2019-01-05 10:31:19 +0100

    Changed to api only

--- Plugins/20 Desktop/10 Automated Pool Desktop Wrong Snapshot.ps1
+++ Plugins/20 Desktop/10 Automated Pool Desktop Wrong Snapshot.ps1
@@ -4,20 +4,26 @@
 
 $wrongsnapdesktops=@()
 foreach ($pool in $pools){
-$poolname=$pool.base.name
-if ($pool.type -like "*automated*" -AND $pool.source -like "*VIEW_COMPOSER*"){
+	$poolname=$pool.base.name
 
-$poolmachines=get-hvmachine ($pool.base.name)
-$wrongsnaps=$poolmachines | where {$_.managedmachinedata.viewcomposerdata.baseimagesnapshotpath -notlike  $pool.automateddesktopdata.VirtualCenternamesdata.snapshotpath -OR $_.managedmachinedata.viewcomposerdata.baseimagepath -notlike $pool.automateddesktopdata.VirtualCenternamesdata.parentvmpath}
-foreach ($wrongsnap in $wrongsnaps){
-$wrongsnapdesktops+= New-Object PSObject -Property @{"VM Name" = $wrongsnap.base.name;
-								"VM Snapshot" = $wrongsnap.managedmachinedata.viewcomposerdata.baseimagesnapshotpath;
-								"VM GI" = $wrongsnap.managedmachinedata.viewcomposerdata.baseimagepath;
-								"Pool Snapshot" = $pool.automateddesktopdata.VirtualCenternamesdata.snapshotpath;
-								"Pool GI" = $pool.automateddesktopdata.VirtualCenternamesdata.parentvmpath;
-}
-}
-}
+	if ($pool.type -like "*automated*"){
+		$queryservice=new-object vmware.hv.queryserviceservice
+		$defn = New-Object VMware.Hv.QueryDefinition
+		$defn.queryentitytype='MachineSummaryView'
+		$filter = New-Object VMware.Hv.QueryFilterEquals -Property @{ 'memberName' = 'base.name'; 'value' = "$pool.base.name" }
+		$queryResults = $queryService.QueryService_Create($Services1, $defn)
+		$poolmachines=$queryResults.results
+		$wrongsnaps=$poolmachines | where {$_.managedmachinedata.viewcomposerdata.baseimagesnapshotpath -notlike  $pool.automateddesktopdata.VirtualCenternamesdata.snapshotpath -OR $_.managedmachinedata.viewcomposerdata.baseimagepath -notlike $pool.automateddesktopdata.VirtualCenternamesdata.parentvmpath}
+		foreach ($wrongsnap in $wrongsnaps){
+			$wrongsnapdesktops+= New-Object PSObject -Property @{
+				"VM Name" = $wrongsnap.base.name;
+				"VM Snapshot" = $wrongsnap.managedmachinedata.viewcomposerdata.baseimagesnapshotpath;
+				"VM GI" = $wrongsnap.managedmachinedata.viewcomposerdata.baseimagepath;
+				"Pool Snapshot" = $pool.automateddesktopdata.VirtualCenternamesdata.snapshotpath;
+				"Pool GI" = $pool.automateddesktopdata.VirtualCenternamesdata.parentvmpath;
+			}
+		}
+	}
 }
 $wrongsnapdesktops
 
@@ -26,5 +32,5 @@ $Header = "VDI Desktops based on wrong snapshot"
 $Comments = "These desktops have not been recomposed with the correct Golden Image Snapshot"
 $Display = "Table"
 $Author = "Wouter Kursten"
-$PluginVersion = 0.2
+$PluginVersion = 0.3
 $PluginCategory = "View"
